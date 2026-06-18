1d9b62f4ffe5eabee0fd6e0f455d323992445e50
sameersmx1-coder <sameersmx1@gmail.com>
2026-06-18 20:58:29 +0530

    simple calculator in python
    simple calculator in python using basic Arithmetic operations

---
+++ smplcalcu.py
@@ -0,0 +1,33 @@
+num1 = float(input("enter first number :"))
+num2 = float(input("enter second number:"))
+
+print("choose operation:")
+print("1. Addition (+)")
+print("2. subtraction (-)")
+print("3. Multiplication (*)")
+print("4.Division (/)")
+
+choice = input("enter your choice (1/2/3/4):")
+
+if choice == '1':
+    result = num1 + num2
+    print("result=" , result)
+
+elif choice == '2':
+    result = num1 - num2
+    print("result=" , result)
+
+elif choice == '3':
+    result = num1 * num2
+    print("result=" , result)
+
+elif choice == '4':
+    if num2 != 0:
+        result = num1 / num2
+        print("result=" , result)
+    else:
+        print("error! division by zero is not allowed")
+else:
+ print("invalid choice!") 
+
+    
